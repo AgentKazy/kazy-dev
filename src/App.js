5c65ce9225837120d767c199e0d9c81455282f77
AgentKazy <kazygaming@outlook.pt>
2023-05-06 02:40:54 +0100

    Website React app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// Styles
 import './App.css';
+
+// Pages & Components
+import Navbar from './components/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+// Styles & Images
+import './Navbar.css';
+import Snowflake from '../assets/snowflake-light.svg';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <ul>
+        <li className="logo">
+          <img src={Snowflake} alt="Kazy website logo" />
+          <span className="brand">Kazy</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
