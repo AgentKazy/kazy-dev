3c80c74d6ffdced27a256a3463c7ccdc9e47c1d3
AgentKazy <kazygaming@outlook.pt>
2023-07-18 10:11:46 +0100

    Progress bar

--- src/assets/svg/LogoNode.js
+++ src/assets/svg/LogoNode.js
@@ -8,7 +8,7 @@ export default function LogoNode() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_52_4784)">
+      <g clipPath="url(#clip0_52_4784)">
         <path
           d="M157.625 195C156.544 195 155.534 194.712 154.597 194.208L145.007 188.522C143.565 187.73 144.286 187.442 144.718 187.298C146.665 186.65 147.026 186.506 149.045 185.354C149.261 185.21 149.549 185.282 149.766 185.426L157.121 189.817C157.409 189.961 157.77 189.961 157.986 189.817L186.756 173.189C187.045 173.045 187.189 172.757 187.189 172.398V139.214C187.189 138.854 187.045 138.566 186.756 138.422L157.986 121.866C157.697 121.722 157.337 121.722 157.121 121.866L128.35 138.422C128.062 138.566 127.917 138.926 127.917 139.214V172.398C127.917 172.686 128.062 173.045 128.35 173.189L136.21 177.724C140.464 179.884 143.132 177.364 143.132 174.845V142.093C143.132 141.661 143.492 141.229 143.997 141.229H147.675C148.107 141.229 148.54 141.589 148.54 142.093V174.845C148.54 180.532 145.439 183.843 140.031 183.843C138.373 183.843 137.075 183.843 133.398 182.043L125.826 177.724C123.952 176.645 122.798 174.629 122.798 172.47V139.286C122.798 137.126 123.952 135.111 125.826 134.031L154.597 117.403C156.4 116.395 158.851 116.395 160.654 117.403L189.424 134.031C191.299 135.111 192.453 137.126 192.453 139.286V172.47C192.453 174.629 191.299 176.645 189.424 177.724L160.654 194.352C159.789 194.712 158.707 195 157.625 195Z"
           fill="#5FA04E"
@@ -22,26 +22,26 @@ export default function LogoNode() {
           fill="#5FA04E"
         />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M68.4293 65.2159C68.4293 63.9203 67.7082 62.6966 66.5545 62.0487L36.0533 44.4131C35.5486 44.1251 34.9717 43.9812 34.3949 43.9092C34.3228 43.9092 34.1065 43.9092 34.1065 43.9092C33.5296 43.9092 32.9528 44.1251 32.448 44.4131L1.87477 61.9767C0.721067 62.6246 0 63.8483 0 65.2159L0.0721067 112.436C0.0721067 113.084 0.43264 113.732 1.00949 114.02C1.58635 114.38 2.30741 114.38 2.81216 114.02L20.983 103.654C22.1368 103.007 22.8578 101.783 22.8578 100.487V78.3887C22.8578 77.093 23.5789 75.8693 24.7326 75.2215L32.448 70.7586C33.0249 70.3987 33.6738 70.2547 34.3228 70.2547C34.9717 70.2547 35.6207 70.3987 36.1255 70.7586L43.8409 75.2215C44.9946 75.8693 45.7156 77.093 45.7156 78.3887V100.487C45.7156 101.783 46.4367 103.007 47.5904 103.654L65.6171 114.02C66.1939 114.38 66.915 114.38 67.4919 114.02C68.0687 113.732 68.4293 113.084 68.4293 112.436V65.2159Z"
           fill="white"
         />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M215.382 0.215947C214.806 -0.0719823 214.085 -0.0719823 213.58 0.215947C213.003 0.575858 212.642 1.15172 212.642 1.79956V48.588C212.642 49.0199 212.426 49.4518 211.993 49.7398C211.561 49.9557 211.128 49.9557 210.696 49.7398L203.052 45.3488C201.899 44.701 200.528 44.701 199.375 45.3488L168.802 62.9845C167.648 63.6323 166.927 64.856 166.927 66.1517V101.351C166.927 102.647 167.648 103.87 168.802 104.518L199.375 122.154C200.528 122.802 201.899 122.802 203.052 122.154L233.625 104.518C234.779 103.87 235.5 102.647 235.5 101.351V13.6047C235.5 12.237 234.779 11.0133 233.625 10.3654L215.382 0.215947ZM212.57 89.7619C212.57 90.1218 212.426 90.4098 212.138 90.5537L201.682 96.6002C201.394 96.7442 201.033 96.7442 200.745 96.6002L190.289 90.5537C190.001 90.4098 189.857 90.0498 189.857 89.7619V77.6689C189.857 77.309 190.001 77.021 190.289 76.8771L200.745 70.8306C201.033 70.6866 201.394 70.6866 201.682 70.8306L212.138 76.8771C212.426 77.021 212.57 77.381 212.57 77.6689V89.7619Z"
           fill="white"
         />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M317.125 77.3809C318.279 76.7331 318.928 75.5094 318.928 74.2137V65.6478C318.928 64.3522 318.207 63.1285 317.125 62.4806L286.769 44.9169C285.615 44.2691 284.245 44.2691 283.091 44.9169L252.518 62.5526C251.364 63.2004 250.643 64.4241 250.643 65.7198V100.919C250.643 102.215 251.364 103.439 252.518 104.086L282.875 121.362C284.028 122.01 285.398 122.01 286.48 121.362L304.867 111.141C305.444 110.853 305.805 110.205 305.805 109.557C305.805 108.909 305.444 108.261 304.867 107.973L274.15 90.3378C273.573 89.9778 273.212 89.402 273.212 88.7542V77.7409C273.212 77.093 273.573 76.4452 274.15 76.1573L283.74 70.6866C284.317 70.3267 285.038 70.3267 285.615 70.6866L295.205 76.1573C295.782 76.5172 296.142 77.093 296.142 77.7409V86.3787C296.142 87.0266 296.503 87.6744 297.08 87.9623C297.657 88.3223 298.378 88.3223 298.955 87.9623L317.125 77.3809Z"
           fill="white"
         />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M284.461 75.7253C284.677 75.5814 284.966 75.5814 285.182 75.7253L291.023 79.1085C291.239 79.2525 291.383 79.4684 291.383 79.7564V86.5227C291.383 86.8106 291.239 87.0266 291.023 87.1705L285.182 90.5537C284.966 90.6977 284.677 90.6977 284.461 90.5537L278.62 87.1705C278.404 87.0266 278.26 86.8106 278.26 86.5227V79.7564C278.26 79.4684 278.404 79.2525 278.62 79.1085L284.461 75.7253Z"
           fill="#5FA04E"
         />
@@ -83,11 +83,11 @@ export default function LogoNode() {
           y2="114.021"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#3F873F" />
-          <stop offset="0.3296" stop-color="#3F8B3D" />
-          <stop offset="0.6367" stop-color="#3E9638" />
-          <stop offset="0.9341" stop-color="#3DA92E" />
-          <stop offset="1" stop-color="#3DAE2B" />
+          <stop stopColor="#3F873F" />
+          <stop offset="0.3296" stopColor="#3F8B3D" />
+          <stop offset="0.6367" stopColor="#3E9638" />
+          <stop offset="0.9341" stopColor="#3DA92E" />
+          <stop offset="1" stopColor="#3DAE2B" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_52_4784"
@@ -97,10 +97,10 @@ export default function LogoNode() {
           y2="30.4091"
           gradientUnits="userSpaceOnUse"
         >
-          <stop offset="0.1376" stop-color="#3F873F" />
-          <stop offset="0.4016" stop-color="#52A044" />
-          <stop offset="0.7129" stop-color="#64B749" />
-          <stop offset="0.9081" stop-color="#6ABF4B" />
+          <stop offset="0.1376" stopColor="#3F873F" />
+          <stop offset="0.4016" stopColor="#52A044" />
+          <stop offset="0.7129" stopColor="#64B749" />
+          <stop offset="0.9081" stopColor="#6ABF4B" />
         </linearGradient>
         <linearGradient
           id="paint2_linear_52_4784"
@@ -110,10 +110,10 @@ export default function LogoNode() {
           y2="38.739"
           gradientUnits="userSpaceOnUse"
         >
-          <stop offset="0.0919165" stop-color="#6ABF4B" />
-          <stop offset="0.2871" stop-color="#64B749" />
-          <stop offset="0.5984" stop-color="#52A044" />
-          <stop offset="0.8624" stop-color="#3F873F" />
+          <stop offset="0.0919165" stopColor="#6ABF4B" />
+          <stop offset="0.2871" stopColor="#64B749" />
+          <stop offset="0.5984" stopColor="#52A044" />
+          <stop offset="0.8624" stopColor="#3F873F" />
         </linearGradient>
         <linearGradient
           id="paint3_linear_52_4784"
@@ -123,10 +123,10 @@ export default function LogoNode() {
           y2="83.2364"
           gradientUnits="userSpaceOnUse"
         >
-          <stop offset="0.0919165" stop-color="#6ABF4B" />
-          <stop offset="0.2871" stop-color="#64B749" />
-          <stop offset="0.5984" stop-color="#52A044" />
-          <stop offset="0.8624" stop-color="#3F873F" />
+          <stop offset="0.0919165" stopColor="#6ABF4B" />
+          <stop offset="0.2871" stopColor="#64B749" />
+          <stop offset="0.5984" stopColor="#52A044" />
+          <stop offset="0.8624" stopColor="#3F873F" />
         </linearGradient>
         <linearGradient
           id="paint4_linear_52_4784"
@@ -136,10 +136,10 @@ export default function LogoNode() {
           y2="103.255"
           gradientUnits="userSpaceOnUse"
         >
-          <stop offset="0.0919165" stop-color="#6ABF4B" />
-          <stop offset="0.2871" stop-color="#64B749" />
-          <stop offset="0.5984" stop-color="#52A044" />
-          <stop offset="0.8624" stop-color="#3F873F" />
+          <stop offset="0.0919165" stopColor="#6ABF4B" />
+          <stop offset="0.2871" stopColor="#64B749" />
+          <stop offset="0.5984" stopColor="#52A044" />
+          <stop offset="0.8624" stopColor="#3F873F" />
         </linearGradient>
         <linearGradient
           id="paint5_linear_52_4784"
@@ -149,10 +149,10 @@ export default function LogoNode() {
           y2="112.303"
           gradientUnits="userSpaceOnUse"
         >
-          <stop offset="0.0919165" stop-color="#6ABF4B" />
-          <stop offset="0.2871" stop-color="#64B749" />
-          <stop offset="0.5984" stop-color="#52A044" />
-          <stop offset="0.8624" stop-color="#3F873F" />
+          <stop offset="0.0919165" stopColor="#6ABF4B" />
+          <stop offset="0.2871" stopColor="#64B749" />
+          <stop offset="0.5984" stopColor="#52A044" />
+          <stop offset="0.8624" stopColor="#3F873F" />
         </linearGradient>
         <linearGradient
           id="paint6_linear_52_4784"
@@ -162,11 +162,11 @@ export default function LogoNode() {
           y2="144.714"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#3F873F" />
-          <stop offset="0.3296" stop-color="#3F8B3D" />
-          <stop offset="0.6367" stop-color="#3E9638" />
-          <stop offset="0.9341" stop-color="#3DA92E" />
-          <stop offset="1" stop-color="#3DAE2B" />
+          <stop stopColor="#3F873F" />
+          <stop offset="0.3296" stopColor="#3F8B3D" />
+          <stop offset="0.6367" stopColor="#3E9638" />
+          <stop offset="0.9341" stopColor="#3DA92E" />
+          <stop offset="1" stopColor="#3DAE2B" />
         </linearGradient>
         <clipPath id="clip0_52_4784">
           <rect width="319" height="195" fill="white" />
