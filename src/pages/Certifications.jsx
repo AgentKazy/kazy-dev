33e8958c066c9b2ae270d89dc0aa9559816ca7d5
AgentKazy <kazygaming@outlook.pt>
2024-11-21 21:13:48 +0000

    Progress update

--- src/pages/Certifications.jsx
+++ src/pages/Certifications.jsx
@@ -103,7 +103,7 @@ export const certs = [
     logoClass: `${styles.anchor}`,
     showProgressText: true,
     progressDescription: "In progress",
-    progress: 25,
+    progress: 29,
     paused: false,
     starred: false,
   },
